cbd08e63f8ddfecf3c81188002065b7c1f39c7e4
myfreeweb <me@myfreeweb.ru>
2010-02-06 22:54:38 +0300

    Using hook

--- django-mode.el
+++ django-mode.el
@@ -1,9 +1,11 @@
-(define-derived-mode django-mode python-mode "Django" "Major mode for Django web framework."
-	(font-lock-add-keywords nil
-				'(("\\(django\\|models\\|request\\)" . font-lock-type-face)
-				  ("\\(get_list_or_404\\|get_object_or_404\\|redirect\\|render_to_response\\)" . font-lock-builtin-face))
-	)
+(define-derived-mode django-mode python-mode "Django" "Major mode for Django web framework.")
+(add-hook 'django-mode-hook
+	  (lambda ()
+	    (font-lock-add-keywords nil
+				    '(("\\<\\(django\\|models\\|request\\)" 1 font-lock-type-face)
+				      ("\\<\\(get_list_or_404\\|get_object_or_404\\|redirect\\|render_to_response\\)" . font-lock-builtin-face))
+				    ))
 )
 
-(add-to-list 'auto-mode-alist '("\\(models.py\\|views.py\\|feeds.py\\|sitemaps.py\\|admin.py\\|urls.py\\|settings.py\\|tests.py\\|assets.py\\)" . django-mode))
+(add-to-list 'auto-mode-alist '("\\<\\(models.py\\|views.py\\|feeds.py\\|sitemaps.py\\|admin.py\\|urls.py\\|settings.py\\|tests.py\\|assets.py\\)" . django-mode))
 ;; django-mode.el ends here
